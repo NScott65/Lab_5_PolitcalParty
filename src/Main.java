8f2450ced154a760ea77b3da5d7038b31a9f39d2
Noah <scott.613865@swl.k12.oh.us>
2023-09-28 09:37:22 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //Variables
+        String choice;
+
+        String menu = "R - Republican\tD - Democrat\tI - Independant\tO - Other.\nChoose your Party: ";
+        System.out.print(menu);
+        choice = scan.nextLine();
+        choice = choice.toUpperCase();
+
+
+
+        if(choice.equals("R")){
+            System.out.print("You get a Republican Elaphant.");
+        }else if(choice.equals("D")){
+            System.out.print("You get a Democrat Donkey.");
+        }else if(choice.equals("I")){
+            System.out.print("You get an Independant Person.");
+        }else if(choice.equals("O")){
+            System.out.print("You get an Other.");
+            }
+        }
     }
-}
